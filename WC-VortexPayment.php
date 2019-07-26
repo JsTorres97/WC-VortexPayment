f61b6e14bad289cd1e4c5b97a80e3d4368965ebd
Jesus Torres <stbazany@gmail.com>
2019-07-26 17:30:23 -0500

    Ya genera la liga y redirecciona, solo falta encontrar la manera de que pueda recibir la respuesta de la transaccion

--- WC-VortexPayment.php
+++ WC-VortexPayment.php
@@ -1,38 +1,38 @@
 <?php
 /*
- * Plugin Name: WooCommerce VortexPayment Gateway
- * Description: Take credit card payments on your store.
- * Author: VortexPayment 
+ * Plugin Name: WooCommerce Vortex Payment Gateway
+ * Plugin URI: http://vortex-solutions.com.mx/
+ * Description: Recibe pagos con tarjeta en tu sitio web con Vortex Payement.
+ * Author: Jesus Torres
  * Author URI: http://vortex-solutions.com.mx/
  * Version: 1.0.0
  */
 /*
  * This action hook registers our PHP class as a WooCommerce payment gateway
  */
-add_filter( 'woocommerce_payment_gateways', 'VortexPayment_add_gateway_class' );
-function VortexPayment_add_gateway_class( $gateways ) {
-	$gateways[] = 'WC_VortexPayment_Gateway'; // your class name is here
+add_filter( 'woocommerce_payment_gateways', 'vortex_add_gateway_class' );
+function vortex_add_gateway_class( $gateways ) {
+	$gateways[] = 'WC_Vortex_Gateway'; // your class name is here
 	return $gateways;
 }
  
 /*
  * The class itself, please note that it is inside plugins_loaded action hook
  */
-add_action( 'plugins_loaded', 'VortexPayment_init_gateway_class' );
-function VortexPayment_init_gateway_class() {
+add_action( 'plugins_loaded', 'vortex_init_gateway_class' );
+function vortex_init_gateway_class() {
  
-	class WC_VortexPayment_Gateway extends WC_Payment_Gateway {
+	class WC_Vortex_Gateway extends WC_Payment_Gateway {
  
  		/**
  		 * Class constructor, more about it in Step 3
  		 */
-          public function __construct() {
+ 		public function __construct() {
  
-            $this->id = 'misha'; // payment gateway plugin ID
-            $this->icon = 'http://vortex-solutions.com.mx/media/icons/VortexPaymentLogo.png'; // URL of the icon that will be displayed on checkout page near your gateway name
-            $this->has_fields = true; // in case you need a custom credit card form
+            $this->id = 'vortex'; // payment gateway plugin ID
+            $this->icon = '../vortex-payment/media/VortexPaymentLogo.png'; // URL of the icon that will be displayed on checkout page near your gateway name
             $this->method_title = 'Vortex Payment Gateway';
-            $this->method_description = 'Acepta todas las tarjetas con Vortex Payment'; // will be displayed on the options page
+            $this->method_description = 'Recibe pagos con tarjeta en tu sitio web con Vortex Payement'; // will be displayed on the options page
          
             // gateways can support subscriptions, refunds, saved payment methods,
             // but in this tutorial we begin with simple payments
@@ -48,236 +48,151 @@ function VortexPayment_init_gateway_class() {
             $this->title = $this->get_option( 'title' );
             $this->description = $this->get_option( 'description' );
             $this->enabled = $this->get_option( 'enabled' );
-            $this->testmode = 'yes' === $this->get_option( 'testmode' );
-            $this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
-            $this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
-         
+			$this->BusinessID = $this->get_option('BID');
+
             // This action hook saves the settings
             add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
          
-            // We need custom JavaScript to obtain a token
-            add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
+            
          
             // You can also register a webhook here
             // add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
-         }
+ 
+ 		}
  
 		/**
  		 * Plugin options, we deal with it in Step 3 too
  		 */
-          public function init_form_fields(){
+ 		public function init_form_fields(){
  
-            $this->form_fields = array(
-                'enabled' => array(
-                    'title'       => 'Activado/Desactivado',
-                    'label'       => 'Activar Vortex Payment Gateway',
-                    'type'        => 'checkbox',
-                    'description' => '',
-                    'default'     => 'yes'
-                ),
-                'title' => array(
-                    'title'       => 'Título',
-                    'type'        => 'text',
-                    'description' => 'Esto controla el título que el usuario ve durante el pago.',
-                    'default'     => 'Vortex Payment',
-                    'desc_tip'    => true,
-                ),
-                'description' => array(
-                    'title'       => 'Descripción',
-                    'type'        => 'textarea',
-                    'description' => 'Esto controla la descripción que el usuario ve durante el proceso de pago.',
-                    'default'     => 'Paga con tu tarjeta de Débito/Crédito.',
-                ),
-                'testmode' => array(
-                    'title'       => 'Modo de prueba',
-                    'label'       => 'Activa el modo de prueba',
-                    'type'        => 'checkbox',
-                    'description' => 'Coloque la pasarela de pago en modo de prueba utilizando claves de API de prueba.',
-                    'default'     => 'yes',
-                    'desc_tip'    => true,
-                ),
-                'test_publishable_key' => array(
-                    'title'       => 'Test Publishable Key',
-                    'type'        => 'text'
-                ),
-                'test_private_key' => array(
-                    'title'       => 'Test Private Key',
-                    'type'        => 'password',
-                ),
-                'publishable_key' => array(
-                    'title'       => 'Live Publishable Key',
-                    'type'        => 'text'
-                ),
-                'private_key' => array(
-                    'title'       => 'Live Private Key',
-                    'type'        => 'password'
-                )
-            );
-        }
+		$this->form_fields = array(
+			'enabled' => array(
+				'title'       => 'Enable/Disable',
+				'label'       => 'Enable Vortex Gateway',
+				'type'        => 'checkbox',
+				'description' => '',
+				'default'     => 'no'
+			),
+			'title' => array(
+				'title'       => 'Title',
+				'type'        => 'text',
+				'description' => 'This controls the title which the user sees during checkout.',
+				'default'     => 'Vortex Payment',
+				'desc_tip'    => true,
+			),
+			'description' => array(
+				'title'       => 'Description',
+				'type'        => 'textarea',
+				'description' => 'This controls the description which the user sees during checkout.',
+				'default'     => 'Paga con Vortex Payment.',
+			),
+			'BID' => array(
+				'title'       => 'Business ID',
+				'type'        => 'text'
+			)
+
+			);
+ 
+	 	}
  
 		/**
 		 * You will need it if you want your custom credit card form, Step 4 is about it
 		 */
 		public function payment_fields() {
  
-            // ok, let's display some description before the payment form
-            if ( $this->description ) {
-                // you can instructions for test mode, I mean test card numbers etc.
-                if ( $this->testmode ) {
-                    $this->description .= ' TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href="#" target="_blank" rel="noopener noreferrer">documentation</a>.';
-                    $this->description  = trim( $this->description );
-                }
-                // display the description with <p> tags etc.
-                echo wpautop( wp_kses_post( $this->description ) );
-            }
-         
-            // I will echo() the form, but you can close PHP tags and print it directly in HTML
-            echo '<fieldset id="wc-' . esc_attr( $this->id ) . '-cc-form" class="wc-credit-card-form wc-payment-form" style="background:transparent;">';
-         
-            // Add this action hook if you want your custom payment gateway to support it
-            do_action( 'woocommerce_credit_card_form_start', $this->id );
-         
-            // I recommend to use inique IDs, because other gateways could already use #ccNo, #expdate, #cvc
-            echo '<div class="form-row form-row-wide"><label>Card Number <span class="required">*</span></label>
-                <input id="VortexPayment_ccNo" type="text" autocomplete="off">
-                </div>
-                <div class="form-row form-row-first">
-                    <label>Expiry Date <span class="required">*</span></label>
-                    <input id="VortexPayment_expdate" type="text" autocomplete="off" placeholder="MM / YY">
-                </div>
-                <div class="form-row form-row-last">
-                    <label>Card Code (CVC) <span class="required">*</span></label>
-                    <input id="VortexPayment_cvv" type="password" autocomplete="off" placeholder="CVC">
-                </div>
-                <div class="clear"></div>';
-         
-            do_action( 'woocommerce_credit_card_form_end', $this->id );
-         
-            echo '<div class="clear"></div></fieldset>';
-         
-        }
+		
+ 
+		}
  
 		/*
 		 * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
 		 */
-        public function payment_scripts() {
+	 	public function payment_scripts() {
  
-            // we need JavaScript to process a token only on cart/checkout pages, right?
-            if ( ! is_cart() && ! is_checkout() && ! isset( $_GET['pay_for_order'] ) ) {
-                return;
-            }
-         
-            // if our payment gateway is disabled, we do not have to enqueue JS too
-            if ( 'no' === $this->enabled ) {
-                return;
-            }
-         
-            // no reason to enqueue JavaScript if API keys are not set
-            if ( empty( $this->private_key ) || empty( $this->publishable_key ) ) {
-                return;
-            }
-         
-            // do not work with card detailes without SSL unless your website is in a test mode
-            if ( ! $this->testmode && ! is_ssl() ) {
-                return;
-            }
-         
-            // let's suppose it is our payment processor JavaScript that allows to obtain a token
-            wp_enqueue_script( 'VortexPayment_js', 'https://www.VortexPaymentpayments.com/api/token.js' );
-         
-            // and this is our custom JS in your plugin directory that works with token.js
-            wp_register_script( 'woocommerce_VortexPayment', plugins_url( 'VortexPayment.js', __FILE__ ), array( 'jquery', 'VortexPayment_js' ) );
-         
-            // in most payment processors you have to use PUBLIC KEY to obtain a token
-            wp_localize_script( 'woocommerce_VortexPayment', 'VortexPayment_params', array(
-                'publishableKey' => $this->publishable_key
-            ) );
-         
-            wp_enqueue_script( 'woocommerce_VortexPayment' );
-         
-        }
+		
+ 
+	 	}
  
 		/*
  		 * Fields validation, more in Step 5
 		 */
-		public function validate_fields(){
+		public function validate_fields() {
  
-            if( empty( $_POST[ 'billing_first_name' ]) ) {
-                wc_add_notice(  'First name is required!', 'error' );
-                return false;
-            }
-            return true;
-         
-        }
+		
+ 
+		}
  
 		/*
 		 * We're processing the payments here, everything about it is in Step 5
 		 */
 		public function process_payment( $order_id ) {
+			
+			global $woocommerce;
  
-            global $woocommerce;
-         
-            // we need it to get any order detailes
-            $order = wc_get_order( $order_id );
-         
-         
-            /*
-              * Array with parameters for API interaction
-              * $args = array('Name');
-             */
-           
-         
-            /*
-             * Your API interaction could be built with wp_remote_post()
-              */
-             $response = add_query_arg( 'key', 'value', 'http://example.com' );
-         
-         
-             if( !is_wp_error( $response ) ) {
-         
-                 $body = json_decode( $response['body'], true );
-         
-                 // it could be different depending on your payment processor
-                 if ( $body['response']['responseCode'] == 'APPROVED' ) {
-         
-                    // we received the payment
-                    $order->payment_complete();
-                    $order->reduce_order_stock();
-         
-                    // some notes to customer (replace true with false to make it private)
-                    $order->add_order_note( 'Hey, your order is paid! Thank you!', true );
-         
-                    // Empty cart
-                    $woocommerce->cart->empty_cart();
-         
-                    // Redirect to the thank you page
-                    return array(
-                        'result' => 'success',
-                        'redirect' => $this->get_return_url( $order )
-                    );
-         
-                 } else {
-                    wc_add_notice(  'Please try again.', 'error' );
-                    return;
-                }
-         
-            } else {
-                wc_add_notice(  'Connection error.', 'error' );
-                return;
-            }
-         
-        }
+			// we need it to get any order detailes
+			$order = wc_get_order( $order_id );
+
+			//Variables para API
+			$url1 = 'https://qaag.mitec.com.mx/praga-ws/url/generateUrlV3';
+            $authorization = "Authorization: Bearer MDhhNzllMjMtNTU1Yy00ZDc3LThmZDctODEyNmVkZTFmODhi";
+            $Key="E166173C2B870BDC3F62A67A77442FE1";
+			$fecha=date("d/m/Y");
+			
+			
+			/*
+			* Array with parameters for API interaction
+			*/
+			$data = array(
+				'ammount' => $order->get_total(),
+				'businessId' => $this->BusinessID,
+				'currency' => 'MXN',
+				'effectiveDate' => $fecha,
+				'id' => $order_id,
+				'paymentTypes' => '401',
+				'reference' => 'Orden'.$order_id,
+				'station' => 'prueba',
+				'userCode' => '1563322091989'
+			);
+			
+			$payload = json_encode($data);
+			
+			$res = encriptar($payload,$Key);
+
+			$ch = curl_init($url1);
+			
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $res);
+
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', $authorization));
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $result = curl_exec($ch);
+			curl_close($ch);
+			$respuesta = json_decode($result);
+			$pag=$respuesta->url;
+			
+			return array(
+				'result' => 'success',
+				'redirect' => $respuesta->url
+			);
+			
  
-		/*
-		 * In case you need a webhook, like PayPal IPN etc
-		 */
-		public function webhook() {
+		
  
-            $order = wc_get_order( $_GET['id'] );
-            $order->payment_complete();
-            $order->reduce_order_stock();
-         
-            update_option('webhook_debug', $_GET);
-        }
- 	}
+		 }
+		
+		 
+		
+
+		
+		 
+
+
+	 }
+	 function encriptar($plaintext, $key128){
+		$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-128-cbc'));
+		$cipherText = openssl_encrypt ( $plaintext, 'AES-128-CBC', hex2bin($key128), 1, $iv);
+		return base64_encode($iv.$cipherText);
+	}
+	 
 }
